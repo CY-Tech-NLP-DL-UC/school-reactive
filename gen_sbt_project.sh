d7b57f3b5a518d7095a50147c49b955ff020817f
Colin Davidson <colin.davidson@hotmail.fr>
2021-03-29 19:28:07 +0200

    Added simple project generator

---
+++ gen_sbt_project.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+mkdir -p src/{main,test}/{java,resources,scala}
+mkdir lib project target
+
+# create an initial build.sbt file
+echo 'name := "TP1"
+version := "1.0"
+scalaVersion := "2.13.0"' > build.sbt
